a235cf98bf5c9bd7a1503fd99e36dd917f1ea003
Floris de Bruin <fdebruin@gmail.com>
2013-04-04 14:53:46 +0200

    Simple FileManager for reading and writing to File

---
+++ Java/FileManager.java
@@ -0,0 +1,48 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileManager {
+	BufferedReader readerIn;
+	String fileIn;
+	FileWriter writerOut;
+	String fileOut;
+	int sizeIn;
+	
+	public FileManager(String fileIn) {
+		this.fileIn = fileIn;
+		try {
+			readerIn = new BufferedReader(new FileReader(fileIn),2);
+		} catch (Throwable e) {
+			System.err.println("There was an error opening the file...");
+			System.exit(1);
+		}	
+	}
+	
+	public FileManager(String fileIn, String fileOut) {
+		this.fileIn = fileIn;
+		this.fileOut = fileOut;
+		try {
+			readerIn = new BufferedReader(new FileReader(fileIn),2);
+			writerOut = new FileWriter(fileOut);
+		} catch (Throwable e) {
+			System.err.println("There was an error opening the file...");
+			System.exit(1);
+		}	
+	}
+	
+	public String readNextLine() {
+		try {
+			return readerIn.readLine();
+		} catch (IOException e) {
+			System.err.println("There was an error reading from file " + fileIn);
+			return "";
+		}
+	}
+	
+	public void terminate() throws IOException {
+		readerIn.close();
+		writerOut.close();
+	}
+}
