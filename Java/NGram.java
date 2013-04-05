b7cc13d1c3755fcd16e9c25370069d3d7266511c
Gijs van Horn <gijsvanhorn@gmail.com>
2013-04-05 14:06:01 +0200

    NGram top frequency print fixed

--- Java/NGram.java
+++ Java/NGram.java
@@ -7,49 +7,82 @@ import java.io.*;
 public class NGram{
 	
 	HashMap<String, Integer> map = new HashMap<String, Integer>();
+	TreeMap<String, Integer> sortedMap;
 	FileManager manager;
 	int nGramSize;
-	
+
 	public NGram(String inputFile, int n) {
 		nGramSize = n;
 		
 		String inputFileName = inputFile.replaceFirst("\\.txt", "");
 		String outputFile = inputFileName + "_" + n + "grams.txt";
 				
-		FileManager manager = new FileManager(inputFile, outputFile);
+		this.manager = new FileManager(inputFile, outputFile);
 		
+		computeNGrams();
 		
+		sortMap(map);
+		
+
+	}
+	
+	public NGram(String inputFile, int n, int m){
+		
+		this(inputFile, n);
+		printTopFrequencies(m);
 	}
 	
 	public void computeNGrams(){
-		
 		Pattern splitPoint = Pattern.compile(" ");
 		
 		String nextLine = manager.readNextLine();
-		
 		String nGram = "";
 		
 		String[] words;
 		
 		while(nextLine != null){
 			words = splitPoint.split(nextLine);
-			int lineSize = words.length - nGramSize - 1;
+			int lineSize = words.length - (nGramSize-1);
 			
-			for(int i=0; i<lineSize; i++){
+			for(int i=0; i < lineSize; i++){
 				nGram = words[i];
-				System.out.println(nGram);
-				for(int j=1; j < nGramSize; i++){
+				for(int j=1; j < nGramSize; j++){
 					nGram = nGram + " " + words[i+j];
 				}
 				addToMap(nGram);
-				
+				//System.out.println(nGram);
 			}
-		
-		
+			nextLine = manager.readNextLine();
 		}
 	}
 	
 	public void addToMap(String nGram){
+		if(map.containsKey(nGram)){
+			int currentValue = map.get(nGram);
+			map.put(nGram, currentValue+1);
+		}else{
+			map.put(nGram, 1);
+		}
+	}
+	
+	public void sortMap(HashMap<String, Integer> map){
+		
+		FrequencyComparator comparator = new FrequencyComparator(map);
+		sortedMap = new TreeMap<String, Integer>(comparator);
+		sortedMap.putAll(map);
 	
 	}
+	
+	public void printTopFrequencies(int m){
+		int i=0;
+		Iterator entries = sortedMap.entrySet().iterator();
+		while(i < m && entries.hasNext()){
+			Map.Entry nGram = (Map.Entry) entries.next();
+			System.out.println(nGram.getKey() + " - " + nGram.getValue());
+			i++;
+		}
+	}
+		
+	
 }
+
